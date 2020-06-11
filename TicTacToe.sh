d289b0a02a64096836b82e7e9bb9ab9468a470eb
unknown <nick.dhruwe@gmail.com>
2020-06-11 20:56:17 +0530

    Added, empty shell selection

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 echo -e "\n   Welcome to Tic-Tac-Toe game.\n"
-
+echo -e "Player vs Computer\n"
 row=3
 function toss(){
 
@@ -10,10 +10,10 @@ function toss(){
 	if (($checkToss == 1 ))
 	then
 		echo -e "\nPlayer 1 plays First"
-		player1=1
+		playerChance=1
 	else
 		echo -e "\nComputer plays First"
-		player1=0
+		playerChance=0
 	fi
 }
 
@@ -22,22 +22,22 @@ function assignSymbol(){
 
 	if (($assign==1))
 	then
-		player1='X'
-		computer='O'
+		playerSymbol='X'
+		computerSymbol='O'
 	else
-		player1='O'
-     	computer='X'
+		playerSymbol='O'
+     	computerSymbol='X'
 	fi
 
-	echo -e "\nPlayer 1 = $player1"
-	echo -e "Computer = $computer\n"
+	echo -e "\nPlayer 1 = $playerSymbol"
+	echo -e "Computer = $computerSymbol\n"
 
 }
 
 
 function resetBoard(){
 
-	board=(1 2 3 4 5 6 7 8 9)
+	board=( 1 2 3 4 5 6 7 8 9 )
 	for (( i=0,j=0;j<$row;i=$(($i+3)),j++ ))
 	do
 		echo -e "\t${board[i]} | ${board[i+1]} | ${board[i+2]}"
@@ -49,7 +49,57 @@ function resetBoard(){
 
 }
 
+function updatedBoard() {
+
+	for (( i=0,j=0;j<$row;i=$(($i+3)),j++ ))
+   do
+      echo -e "\t${board[i]} | ${board[i+1]} | ${board[i+2]}"
+      if (($j!=$(($row-1))))
+      then
+         echo -e "\t---------"
+      fi
+   done
+
+
+}
+function playersInput() {
+
+	if (( $playerChance == 1 ))
+	then
+		echo -e "\nPlayers 1's Turn:"
+		while ((1))
+		do
+			read -p "Select shell number: " shellNumber
+			if (($shellNumber!=${board[$shellNumber-1]}))
+			then
+				echo -e "Invalid: Shell is already occupied, choose again"
+			else
+				break
+			fi
+		done
+		board[$shellNumber-1]=$playerSymbol
+		updatedBoard
+	else
+		echo -e "\nComputer's turn,"
+		while ((1))
+		do
+			shellNumber=$((RANDOM%9+1))
+			if (($shellNumber==${board[$shellNumber-1]}))
+			then
+				break
+			fi
+		done
+		echo -e "Computer Selected shell number:$shellNumber"
+		board[$shellNumber-1]=$computerSymbol
+		updatedBoard
+	fi
+
+
+}
+
 #****** Main Method *****
-resetBoard
 toss
 assignSymbol
+resetBoard
+playersInput
+#updatedBoard
