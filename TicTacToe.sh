3e9c7aaa6bb9f7185b9e38913d2652054f8b0998
unknown <nick.dhruwe@gmail.com>
2020-06-11 18:40:22 +0530

    Added, board reset.

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,3 +1,19 @@
 #!/bin/bash -x
 
-echo "       Welcome to Tic-Tac-Toe game."
+echo -e "       Welcome to Tic-Tac-Toe game.\n"
+
+row=3
+function resetBoard(){
+
+	board=(1 2 3 4 5 6 7 8 9)
+	for (( i=0,j=0;j<$row;i=$(($i+3)),j++ ))
+	do
+		echo -e "\t${board[i]} | ${board[i+1]} | ${board[i+2]}"
+		if (($j!=$(($row-1))))
+		then
+			echo -e "\t---------"
+		fi
+	done
+
+}
+resetBoard
