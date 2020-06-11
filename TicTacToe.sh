afc55a233f52bab42e5ff3049c291fdcd592cb47
unknown <nick.dhruwe@gmail.com>
2020-06-11 18:50:16 +0530

    Added, Toss

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,8 +1,22 @@
 #!/bin/bash -x
 
-echo -e "       Welcome to Tic-Tac-Toe game.\n"
+echo -e "\n\tWelcome to Tic-Tac-Toe game.\n"
 
 row=3
+function toss(){
+
+	checkToss=$((RANDOM%2))
+
+	if (($checkToss == 1 ))
+	then
+		echo -e "\nPlayer 1 plays First"
+		player1=1
+	else
+		echo -e "\nComputer plays First"
+		player1=0
+	fi
+}
+
 function resetBoard(){
 
 	board=(1 2 3 4 5 6 7 8 9)
@@ -16,4 +30,6 @@ function resetBoard(){
 	done
 
 }
+#****** Main Method *****
 resetBoard
+toss
