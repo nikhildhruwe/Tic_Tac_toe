a1643817fefd515da2023648fe92cb3ca1c9d081
unknown <nick.dhruwe@gmail.com>
2020-06-12 03:50:54 +0530

    Added,win or tie

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -2,7 +2,11 @@
 
 echo -e "\n   Welcome to Tic-Tac-Toe game.\n"
 echo -e "Player vs Computer\n"
+
 row=3
+column=3
+numberOfShell=$(($row*$column))
+
 function toss(){
 
 	checkToss=$((RANDOM%2))
@@ -45,6 +49,8 @@ function resetBoard(){
 		then
 			echo -e "\t---------"
 		fi
+
+
 	done
 
 }
@@ -62,24 +68,29 @@ function updatedBoard() {
 
 
 }
-function playersInput() {
+function playerInput() {
 
-	if (( $playerChance == 1 ))
-	then
 		echo -e "\nPlayers 1's Turn:"
 		while ((1))
 		do
 			read -p "Select shell number: " shellNumber
-			if (($shellNumber!=${board[$shellNumber-1]}))
+			if (( ($shellNumber<1) || ($shellNumber>9)))
+			then
+				echo "Invalid, input 1...9"
+			elif (($shellNumber!=${board[$shellNumber-1]}))
 			then
 				echo -e "Invalid: Shell is already occupied, choose again"
+
 			else
-				break
+					break
 			fi
 		done
 		board[$shellNumber-1]=$playerSymbol
 		updatedBoard
-	else
+}
+
+function computerInput() {
+
 		echo -e "\nComputer's turn,"
 		while ((1))
 		do
@@ -92,14 +103,97 @@ function playersInput() {
 		echo -e "Computer Selected shell number:$shellNumber"
 		board[$shellNumber-1]=$computerSymbol
 		updatedBoard
-	fi
+
+}
+function checkWinCombination(){
+
+		local symbol=$1
+
+	if [ ${board[0]} == $symbol ] && [ ${board[1]} == $symbol ] && [ ${board[2]} == $symbol ]
+   then
+      echo 1
+   elif [ ${board[3]} == $symbol ] && [ ${board[4]} == $symbol ] && [ ${board[5]} == $symbol ]
+   then
+      echo 1
+   elif [ ${board[6]} == $symbol ] && [ ${board[7]} == $symbol ] && [ ${board[8]} == $symbol ]
+   then
+      echo 1
+   elif [ ${board[0]} == $symbol ] && [ ${board[3]} == $symbol ] && [ ${board[6]} == $symbol ]
+   then
+      echo 1
+   elif [ ${board[1]} == $symbol ] && [ ${board[4]} == $symbol ] && [ ${board[7]} == $symbol ]
+   then
+      echo 1
+   elif [ ${board[2]} == $symbol ] && [ ${board[5]} == $symbol ] && [ ${board[8]} == $symbol ]
+   then
+      echo 1
+   elif [ ${board[0]} == $symbol ] && [ ${board[4]} == $symbol ] && [ ${board[8]} == $symbol ]
+   then
+      echo 1
+   elif [ ${board[2]} == $symbol ] && [ ${board[4]} == $symbol ] && [ ${board[6]} == $symbol ]
+   then
+      echo 1
+   else
+      echo 0
+   fi
+
+
 
 
 }
-
 #****** Main Method *****
 assignSymbol
 toss
 resetBoard
-playersInput
-#updatedBoard
+tie=1
+if (( $playerChance==1 ))
+then
+	for ((k=0;$k<$numberOfShell;k++))
+	do
+			playerInput
+			checkWin=$(checkWinCombination $playerSymbol)
+			if (( $checkWin == 1 ))
+			then
+				tie=0
+				echo -e "Player Won"
+				break;
+			fi
+
+
+			computerInput
+			checkWin=$(checkWinCombination $computerSymbol)
+			if (( $checkWin == 1 ))
+			then
+				tie=0
+				echo -e "Computer Won"
+				break
+			fi
+
+	done
+else
+	for ((k=0;$k<$numberOfShell;k++))
+	do
+			computerInput
+			checkWin=$(checkWinCombination $computerSymbol)
+         if (( $checkWin == 1 ))
+         then
+				tie=0
+				echo -e "Computer Won"
+            break
+         fi
+			playerInput
+
+			checkWin=$(checkWinCombination $playerSymbol)
+         if (( $checkWin == 1 ))
+         then
+				tie=0
+				echo -e "Player Won"
+            break;
+         fi
+	done
+fi
+
+if (($tie==1))
+then
+	echo "It is a tie"
+fi
